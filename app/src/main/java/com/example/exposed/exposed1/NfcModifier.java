4ff22971271287f205539da94e4780bb2a88f3bd
danidin <ddd@aa>
2017-08-24 19:14:40 +0300

    Add a broadcast reciever and attach it to librelink.
    // To activate it run: adb shell am broadcast -a com.example.Broadcast

--- app/src/main/java/com/example/exposed/exposed1/NfcModifier.java
+++ app/src/main/java/com/example/exposed/exposed1/NfcModifier.java
@@ -1,8 +1,15 @@
 package com.example.exposed.exposed1;
 
+import android.app.AndroidAppHelper;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.widget.TextView;
+
+import android.content.IntentFilter;
+import android.content.BroadcastReceiver;
+import android.os.Bundle;
 
 import static de.robv.android.xposed.SELinuxHelper.getContext;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
@@ -10,6 +17,16 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import de.robv.android.xposed.XposedBridge;
+
+class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+        XposedBridge.log("we are inside the broadcast reciever");
+
+    }
+
+}
 
 public class NfcModifier implements IXposedHookLoadPackage {
 
@@ -81,6 +98,26 @@ public class NfcModifier implements IXposedHookLoadPackage {
             }
         });
 
+
+        // Hook the SplashActivity.oncreate method, to register a broadcast receiver.
+        findAndHookMethod("com.librelink.app.ui.SplashActivity", lpparam.classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                XposedBridge.log("We are before SplashActivity.onCreate " + param.args[0] );
+
+
+                // Register a broadcast reciever
+                Context context = (Context) AndroidAppHelper.currentApplication();
+               XposedBridge.log("context = " + context);
+
+               IntentFilter filter = new IntentFilter("com.example.Broadcast");
+
+               MyReceiver receiver = new MyReceiver();
+               context.registerReceiver(receiver, filter);
+
+               //?? intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+           }
+       });
 
     }
 
