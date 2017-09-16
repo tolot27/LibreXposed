5556b830973c4d35c94c1f102f8a3985e24f8ce8
Tzachi Dar <tzachi.dar@gmail.com>
2017-09-16 23:59:57 +0300

    First version that is able to process the entire data.

--- app/src/main/java/com/example/exposed/exposed1/NfcModifier.java
+++ app/src/main/java/com/example/exposed/exposed1/NfcModifier.java
@@ -22,71 +22,12 @@ import de.robv.android.xposed.XposedBridge;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
-class MyReceiver extends BroadcastReceiver {
-
-    ClassLoader classLoader_;
-    MyReceiver(ClassLoader classLoader) {
-        classLoader_ = classLoader;
-    }
-
-    Object createApplicationRegion() {
-        Class<?> ApplicationRegionDef = XposedHelpers.findClass("com.abbottdiabetescare.flashglucose.sensorabstractionservice.ApplicationRegion", classLoader_);
-        XposedBridge.log("ApplicationRegionDef =  " + ApplicationRegionDef );
-
-        Object ApplicationRegionInstance = XposedHelpers.newInstance(ApplicationRegionDef,"applicationRegion2", 2, 3);
-        XposedBridge.log("ApplicationRegionInstance =  " + ApplicationRegionInstance );
-
-        //Object intRes = XposedHelpers.callMethod(ApplicationRegionInstance, "toValue");
-        //XposedBridge.log("ApplicationRegionInstance toValue returned =  " + intRes );
-
-        Object intRes1 = XposedHelpers.callMethod(ApplicationRegionInstance, "fromValue", 0);
-        XposedBridge.log("ApplicationRegionInstance fromValue returned =  " + intRes1 );
-
-        return intRes1;
-    }
-
-
-    @Override
-    public void onReceive(Context arg0, Intent arg1) {
-        XposedBridge.log("we are inside the broadcast reciever");
-
-        // Create DataProcessingNative
-        Class<?> DataProcessingNativeDef = XposedHelpers.findClass("com.abbottdiabetescare.flashglucose.sensorabstractionservice.dataprocessing.DataProcessingNative", classLoader_);
-        XposedBridge.log("DataProcessingNativeDef =  " + DataProcessingNativeDef );
-        Object DataProcessingNativeInstance = XposedHelpers.newInstance(DataProcessingNativeDef, 1095774808);
-        XposedBridge.log("DataProcessingNativeInstance =  " + DataProcessingNativeInstance );
-        Object ApplicationRegionInstance = createApplicationRegion();
-
-        byte[] bDat = {(byte)0xdf, 0x00, 0x00, 0x01, 01, 02};
-        boolean ret = (boolean)XposedHelpers.callMethod(DataProcessingNativeInstance, "isPatchSupported", bDat, ApplicationRegionInstance);
-        XposedBridge.log("return from  isPatchSupported is " + ret);
-    }
-
-}
-
-public class NfcModifier implements IXposedHookLoadPackage {
-
-
-    public static String byteArrayToHex(byte[] a) {
-        StringBuilder sb = new StringBuilder(a.length * 2);
-        for(byte b: a)
-            sb.append(String.format("0x%02x ", b));
-        return sb.toString();
-    }
-
-    public static boolean comparePartialByteArray(byte []b1, byte[] b2) {
-        int len = Math.min(b1.length, b2.length);
-        for (int i = 0 ; i < len ; i++) {
-            if(b1[i] != b2[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
+class Printer { 
+    // Use reflection print the value of an object
     public static String objectToString (Object obj) {
         StringBuilder result = new StringBuilder();
         String newLine = System.getProperty("line.separator");
@@ -117,6 +58,173 @@ public class NfcModifier implements IXposedHookLoadPackage {
 
         return result.toString();
     }
+}
+
+class MyReceiver extends BroadcastReceiver {
+
+    ClassLoader classLoader_;
+    MyReceiver(ClassLoader classLoader) {
+        classLoader_ = classLoader;
+    }
+
+    Object createApplicationRegion() {
+        Class<?> ApplicationRegionDef = XposedHelpers.findClass("com.abbottdiabetescare.flashglucose.sensorabstractionservice.ApplicationRegion", classLoader_);
+        XposedBridge.log("ApplicationRegionDef =  " + ApplicationRegionDef );
+
+        Object ApplicationRegionInstance = XposedHelpers.newInstance(ApplicationRegionDef,"applicationRegion2", 2, 3);
+        XposedBridge.log("ApplicationRegionInstance =  " + ApplicationRegionInstance );
+
+        //Object intRes = XposedHelpers.callMethod(ApplicationRegionInstance, "toValue");
+        //XposedBridge.log("ApplicationRegionInstance toValue returned =  " + intRes );
+
+        Object intRes1 = XposedHelpers.callMethod(ApplicationRegionInstance, "fromValue", 0);
+        XposedBridge.log("ApplicationRegionInstance fromValue returned =  " + intRes1 );
+
+        return intRes1;
+    }
+
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+        XposedBridge.log("we are inside the broadcast reciever");
+
+        // Create DataProcessingNative
+        Class<?> DataProcessingNativeDef = XposedHelpers.findClass("com.abbottdiabetescare.flashglucose.sensorabstractionservice.dataprocessing.DataProcessingNative", classLoader_);
+        XposedBridge.log("DataProcessingNativeDef =  " + DataProcessingNativeDef );
+        Object DataProcessingNativeInstance = XposedHelpers.newInstance(DataProcessingNativeDef, 1095774808);
+        XposedBridge.log("DataProcessingNativeInstance =  " + DataProcessingNativeInstance );
+        Object ApplicationRegionInstance = createApplicationRegion();
+
+        byte[] bDat = {(byte)0xdf, 0x00, 0x00, 0x01, 01, 02};
+        boolean ret = (boolean)XposedHelpers.callMethod(DataProcessingNativeInstance, "isPatchSupported", bDat, ApplicationRegionInstance);
+        XposedBridge.log("return from  isPatchSupported is " + ret);
+
+        // Create AlarmConfiguration
+        Class<?> AlarmConfigurationDef = XposedHelpers.findClass("com.abbottdiabetescare.flashglucose.sensorabstractionservice.AlarmConfiguration", classLoader_);
+        XposedBridge.log("AlarmConfigurationDef =  " + AlarmConfigurationDef );
+        Object AlarmConfigurationInstance = XposedHelpers.newInstance(AlarmConfigurationDef, 70, 180);
+        XposedBridge.log("AlarmConfigurationInstance =  " + AlarmConfigurationInstance );
+
+        // Create NonActionableConfiguration
+        Class<?> NonActionableConfigurationDef = XposedHelpers.findClass("com.abbottdiabetescare.flashglucose.sensorabstractionservice.NonActionableConfiguration", classLoader_);
+        XposedBridge.log("NonActionableConfigurationDef =  " + NonActionableConfigurationDef );
+        Object NonActionableConfigurationInstance = XposedHelpers.newInstance(NonActionableConfigurationDef, true, true, 0, 40, 500, -2, 2);
+        XposedBridge.log("NonActionableConfigurationInstance =  " + NonActionableConfigurationInstance );
+
+        byte[] packet = {(byte)0x3a, (byte)0xcf, (byte)0x10, (byte)0x16, (byte)0x03, (byte)0x00, (byte)0x00, (byte)0x00,
+                (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00,
+                (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00,
+                (byte)0x4f, (byte)0x11, (byte)0x08, (byte)0x10, (byte)0xad, (byte)0x02, (byte)0xc8, (byte)0xd4,
+                (byte)0x5b, (byte)0x00, (byte)0xaa, (byte)0x02, (byte)0xc8, (byte)0xb4, (byte)0x1b, (byte)0x80,
+                (byte)0xa9, (byte)0x02, (byte)0xc8, (byte)0x9c, (byte)0x5b, (byte)0x00, (byte)0xa9, (byte)0x02,
+                (byte)0xc8, (byte)0x8c, (byte)0x1b, (byte)0x80, (byte)0xb0, (byte)0x02, (byte)0xc8, (byte)0x30,
+                (byte)0x5c, (byte)0x80, (byte)0xb0, (byte)0x02, (byte)0x88, (byte)0xe6, (byte)0x9c, (byte)0x80,
+                (byte)0xb8, (byte)0x02, (byte)0xc8, (byte)0x3c, (byte)0x9d, (byte)0x80, (byte)0xb8, (byte)0x02,
+                (byte)0xc8, (byte)0x60, (byte)0x9d, (byte)0x80, (byte)0xa1, (byte)0x02, (byte)0xc8, (byte)0xdc,
+                (byte)0x9e, (byte)0x80, (byte)0xab, (byte)0x02, (byte)0xc8, (byte)0x14, (byte)0x9e, (byte)0x80,
+                (byte)0xa9, (byte)0x02, (byte)0xc8, (byte)0xc0, (byte)0x9d, (byte)0x80, (byte)0xab, (byte)0x02,
+                (byte)0xc8, (byte)0x78, (byte)0x9d, (byte)0x80, (byte)0xaa, (byte)0x02, (byte)0xc8, (byte)0x40,
+                (byte)0x9d, (byte)0x80, (byte)0xa8, (byte)0x02, (byte)0xc8, (byte)0x08, (byte)0x9d, (byte)0x80,
+                (byte)0xa8, (byte)0x02, (byte)0xc8, (byte)0x2c, (byte)0x5c, (byte)0x80, (byte)0xad, (byte)0x02,
+                (byte)0xc8, (byte)0xf8, (byte)0x5b, (byte)0x00, (byte)0x29, (byte)0x06, (byte)0xc8, (byte)0xf4,
+                (byte)0x9b, (byte)0x80, (byte)0xc9, (byte)0x05, (byte)0xc8, (byte)0x8c, (byte)0xde, (byte)0x80,
+                (byte)0xc3, (byte)0x05, (byte)0xc8, (byte)0x28, (byte)0x9e, (byte)0x80, (byte)0x2c, (byte)0x06,
+                (byte)0xc8, (byte)0xd0, (byte)0x9e, (byte)0x80, (byte)0x7b, (byte)0x06, (byte)0x88, (byte)0xa6,
+                (byte)0x9e, (byte)0x80, (byte)0xf9, (byte)0x05, (byte)0xc8, (byte)0xb0, (byte)0x9e, (byte)0x80,
+                (byte)0x99, (byte)0x05, (byte)0xc8, (byte)0xf0, (byte)0x9e, (byte)0x80, (byte)0x2e, (byte)0x05,
+                (byte)0xc8, (byte)0x00, (byte)0x9f, (byte)0x80, (byte)0x81, (byte)0x04, (byte)0xc8, (byte)0x48,
+                (byte)0xa0, (byte)0x80, (byte)0x5d, (byte)0x04, (byte)0xc8, (byte)0x38, (byte)0x9d, (byte)0x80,
+                (byte)0x12, (byte)0x04, (byte)0xc8, (byte)0x10, (byte)0x9e, (byte)0x80, (byte)0xcf, (byte)0x03,
+                (byte)0xc8, (byte)0x4c, (byte)0x9e, (byte)0x80, (byte)0x6f, (byte)0x03, (byte)0xc8, (byte)0xb8,
+                (byte)0x9e, (byte)0x80, (byte)0x19, (byte)0x03, (byte)0xc8, (byte)0x40, (byte)0x9f, (byte)0x80,
+                (byte)0xc5, (byte)0x02, (byte)0xc8, (byte)0xf4, (byte)0x9e, (byte)0x80, (byte)0xaa, (byte)0x02,
+                (byte)0xc8, (byte)0xf8, (byte)0x5b, (byte)0x00, (byte)0xa2, (byte)0x04, (byte)0xc8, (byte)0x38,
+                (byte)0x9a, (byte)0x00, (byte)0xd1, (byte)0x04, (byte)0xc8, (byte)0x28, (byte)0x9b, (byte)0x80,
+                (byte)0xe4, (byte)0x04, (byte)0xc8, (byte)0xe0, (byte)0x1a, (byte)0x80, (byte)0x8f, (byte)0x04,
+                (byte)0xc8, (byte)0x20, (byte)0x9b, (byte)0x80, (byte)0x22, (byte)0x06, (byte)0xc8, (byte)0x50,
+                (byte)0x5b, (byte)0x80, (byte)0xbc, (byte)0x06, (byte)0xc8, (byte)0x54, (byte)0x9c, (byte)0x80,
+                (byte)0x7f, (byte)0x05, (byte)0xc8, (byte)0x24, (byte)0x5c, (byte)0x80, (byte)0xc9, (byte)0x05,
+                (byte)0xc8, (byte)0x38, (byte)0x5c, (byte)0x80, (byte)0x38, (byte)0x05, (byte)0xc8, (byte)0xf4,
+                (byte)0x1a, (byte)0x80, (byte)0x37, (byte)0x07, (byte)0xc8, (byte)0x84, (byte)0x5b, (byte)0x80,
+                (byte)0xfb, (byte)0x08, (byte)0xc8, (byte)0x4c, (byte)0x9c, (byte)0x80, (byte)0xfb, (byte)0x09,
+                (byte)0xc8, (byte)0x7c, (byte)0x9b, (byte)0x80, (byte)0x77, (byte)0x0a, (byte)0xc8, (byte)0xe4,
+                (byte)0x5a, (byte)0x80, (byte)0xdf, (byte)0x09, (byte)0xc8, (byte)0x88, (byte)0x9f, (byte)0x80,
+                (byte)0x6d, (byte)0x08, (byte)0xc8, (byte)0x2c, (byte)0x9f, (byte)0x80, (byte)0xc3, (byte)0x06,
+                (byte)0xc8, (byte)0xb0, (byte)0x9d, (byte)0x80, (byte)0xd9, (byte)0x11, (byte)0x00, (byte)0x00,
+                (byte)0x72, (byte)0xc2, (byte)0x00, (byte)0x08, (byte)0x82, (byte)0x05, (byte)0x09, (byte)0x51,
+                (byte)0x14, (byte)0x07, (byte)0x96, (byte)0x80, (byte)0x5a, (byte)0x00, (byte)0xed, (byte)0xa6,
+                (byte)0x0e, (byte)0x6e, (byte)0x1a, (byte)0xc8, (byte)0x04, (byte)0xdd, (byte)0x58, (byte)0x6d};
+        int sensorStartTimestamp = 0x0e181349;
+        int sensorScanTimestamp = 0x0e1c4794;
+        int currentUtcOffset = 0x0036ee80;
+        byte[] oldState = {(byte)0xd5, (byte)0x11, (byte)0x00, (byte)0x00, (byte)0xb8, (byte)0x25, (byte)0xb5, (byte)0x94,
+                (byte)0x94, (byte)0x56, (byte)0xcc, (byte)0x36, (byte)0x25, (byte)0xec, (byte)0x4b, (byte)0x40,
+                (byte)0xd5, (byte)0x11, (byte)0x00, (byte)0x00, (byte)0x38, (byte)0xee, (byte)0xe9, (byte)0xae,
+                (byte)0x6b, (byte)0x09, (byte)0x20, (byte)0x2b, (byte)0xfe, (byte)0x44, (byte)0xdc, (byte)0xbf};
+
+        Object DataProcessingOutputsInstance =  XposedHelpers.callMethod(DataProcessingNativeInstance, "processScan", AlarmConfigurationInstance, NonActionableConfigurationInstance,
+                packet, sensorStartTimestamp, sensorScanTimestamp, currentUtcOffset, oldState);
+        XposedBridge.log("return from  processScan is " + DataProcessingOutputsInstance + " " + Printer.objectToString(DataProcessingOutputsInstance));
+
+        java.lang.reflect.Method method = null;
+        Object AlgorithmResultsInstance = null;
+        Object GlucoseValueInstance = null;
+
+
+        try {
+            method = DataProcessingOutputsInstance.getClass().getMethod("getAlgorithmResults");
+        } catch (NoSuchMethodException e) {
+            XposedBridge.log("NoSuchMethodException: Exception cought in getAlgorithmResults" + e);
+        }
+        try {
+            AlgorithmResultsInstance = method.invoke(DataProcessingOutputsInstance);
+        } catch (IllegalAccessException e) {
+            XposedBridge.log("IllegalAccessException: Exception cought in getRealTimeGlucose" + e);
+        } catch (InvocationTargetException e) {
+            XposedBridge.log("InvocationTargetException: Exception cought in getRealTimeGlucose" + e);
+        }
+        XposedBridge.log("return from  getAlgorithmResults is AlgorithmResultsInstance = " + AlgorithmResultsInstance + " " + Printer.objectToString(AlgorithmResultsInstance));
+
+
+
+        try {
+            method = AlgorithmResultsInstance.getClass().getMethod("getRealTimeGlucose");
+        } catch (NoSuchMethodException e) {
+            XposedBridge.log("NoSuchMethodException: Exception cought in getRealTimeGlucose" + e);
+        }
+        try {
+            GlucoseValueInstance = method.invoke(AlgorithmResultsInstance);
+        } catch (IllegalAccessException e) {
+            XposedBridge.log("IllegalAccessException: Exception cought in getRealTimeGlucose" + e);
+        } catch (InvocationTargetException e) {
+            XposedBridge.log("InvocationTargetException: Exception cought in getRealTimeGlucose" + e);
+        }
+        XposedBridge.log("return from  processScan is getRealTimeGlucose GlucoseValueInstance = " + GlucoseValueInstance + " " + Printer.objectToString(GlucoseValueInstance));
+
+    }
+
+}
+
+public class NfcModifier implements IXposedHookLoadPackage {
+
+
+    public static String byteArrayToHex(byte[] a) {
+        StringBuilder sb = new StringBuilder(a.length * 2);
+        for(byte b: a)
+            sb.append(String.format("0x%02x ", b));
+        return sb.toString();
+    }
+
+    public static boolean comparePartialByteArray(byte []b1, byte[] b2) {
+        int len = Math.min(b1.length, b2.length);
+        for (int i = 0 ; i < len ; i++) {
+            if(b1[i] != b2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 
 
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
@@ -180,7 +288,7 @@ public class NfcModifier implements IXposedHookLoadPackage {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 XposedBridge.log("We are before DataProcessingNative.isPatchSupported " + param.args[0] + " " +  param.args[1] + " this = "+ param.thisObject);
-                XposedBridge.log("continuing bytes =  " + byteArrayToHex((byte[])param.args[0]) + "this = " + objectToString(param.thisObject));
+                XposedBridge.log("continuing bytes =  " + byteArrayToHex((byte[])param.args[0]) + "this = " + Printer.objectToString(param.thisObject));
             }
 
         });
@@ -195,6 +303,8 @@ public class NfcModifier implements IXposedHookLoadPackage {
                         XposedBridge.log("We are before DataProcessingNative.processScan AlarmConfiguration " + param.args[0] + " NonActionableConfiguration " + param.args[1] +
                                 " packet " + param.args[2] + " sensorStartTimestamp " + param.args[3] + " sensorScanTimestamp " + param.args[4] +
                                 " currentUtcOffset " + param.args[5] + " oldState " + param.args[6] +  "this = " + param.thisObject);
+                        XposedBridge.log("continuing before DataProcessingNative.processScan AlarmConfiguration " +  Printer.objectToString(param.args[0]) +
+                        " NonActionableConfiguration " + Printer.objectToString(param.args[1]));
                     }
 
                 });
